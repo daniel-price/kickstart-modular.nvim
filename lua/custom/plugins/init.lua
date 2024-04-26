03cc1c6ee259261711b565296a419db4af855581
Daniel Price <64694785+daniel-price@users.noreply.github.com>
2024-04-26 13:27:37 +0100

    feat: replace zellij plugin with tmux

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -16,10 +16,23 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
   },
 
+  -- {
+  --   'Lilja/zellij.nvim',
+  --   opts = {},
+  -- },
   {
-    'Lilja/zellij.nvim',
-    opts = {
-      vimTmuxNavigatorKeybinds = false, -- Will set keybinds like <C-h> to left
-    },
+    'alexghergh/nvim-tmux-navigation',
+    config = function()
+      local nvim_tmux_nav = require 'nvim-tmux-navigation'
+
+      nvim_tmux_nav.setup {
+        disable_when_zoomed = true, -- defaults to false
+      }
+
+      vim.keymap.set('n', '<C-h>', nvim_tmux_nav.NvimTmuxNavigateLeft)
+      vim.keymap.set('n', '<C-j>', nvim_tmux_nav.NvimTmuxNavigateDown)
+      vim.keymap.set('n', '<C-k>', nvim_tmux_nav.NvimTmuxNavigateUp)
+      vim.keymap.set('n', '<C-l>', nvim_tmux_nav.NvimTmuxNavigateRight)
+    end,
   },
 }
