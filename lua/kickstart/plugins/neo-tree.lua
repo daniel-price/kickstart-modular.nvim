0f7bd7adfd81ae4200f83efb1426ee8dcde39bbf
Daniel Price <64694785+daniel-price@users.noreply.github.com>
2024-05-03 08:15:38 +0100

    fix: add neotree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -18,6 +18,14 @@ return {
       window = {
         mappings = {
           ['\\'] = 'close_window',
+          ['<space>'] = false, -- disable space until we figure out which-key disabling
+          ['[b'] = 'prev_source',
+          [']b'] = 'next_source',
+          O = 'system_open',
+          Y = 'copy_selector',
+          h = 'parent_or_close',
+          l = 'child_or_open',
+          o = 'open',
         },
       },
     },
